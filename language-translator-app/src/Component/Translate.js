636ce3e85defc4e349157098a65afde0cc45aa30
balajiav <avenkatabalaji1999@gmail.com>
2023-08-11 09:23:07 +0000

    Added UI part and styles to it

---
+++ language-translator-app/src/Component/Translate.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import "./App.css";
+import {
+    Form,
+    TextArea,
+    Button,
+    Icon
+} from 'semantic-ui-react';
+
+export default function Translate() {
+    
+    return (
+        <div>
+            <div className="app-header">
+                <h2 className="header"> Translator</h2>
+                <Icon className='translate-icon' name='translate' />
+            </div>
+
+            <div className='app-body'>
+                    <Form>
+                        <Form.Field
+                            control={TextArea}
+                            placeholder='Type Text to Translate..'
+                        />
+
+                        <select className="language-select">
+                            <option>Please Select Language..</option>
+                            <option>Telugu</option>
+                            <option>English</option>
+                        </select>
+                        
+
+                        <Form.Field
+                            control={TextArea}
+                            placeholder='Your Result Translation..'
+                        />
+
+                        <Button
+                            color="orange"
+                            size="large"
+                        >
+                            <Icon name='translate' />
+                            Translate</Button>
+                    </Form>
+            </div>
+            <div className='footer'>
+                <p color="black">Please visit again....</p>
+            </div>
+        </div>
+    )
+}
